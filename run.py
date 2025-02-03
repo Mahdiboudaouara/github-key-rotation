4e36fc737873238355e176f34162ac584ae68d54
Mahdiboudaouara <mahdi.boudaouara@supcom.tn>
2025-02-03 23:59:44 +0100

    version1

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python3
 import subprocess, os, requests, shutil
 from pathlib import Path
+from mail import send_email
+
 
 user_profile = os.environ.get('USERPROFILE')
 ssh_folder = Path(f"{user_profile}\\.ssh")
-keyPath = Path(f"{ssh_folder}//my_ssh_key")
+keyPath = Path(f"{ssh_folder}//id_rsa")
 url = "https://api.github.com/user/keys"
 
 #backing up old ssh 
 if keyPath.exists():
-    shutil.copy(keyPath, Path(f"{ssh_folder}\\my_old_ssh_key"))
+    shutil.copy(keyPath, Path(f"{ssh_folder}\\id_rsa_old"))
 
 with open('./github_pat.txt', 'r') as file:
     github_pat = file.read()
@@ -23,7 +26,7 @@ except subprocess.CalledProcessError as e:
     print(f"An error occurred: {e}")
 
 
-with open(f'{user_profile}\\.ssh\\my_ssh_key.pub', 'r') as file:
+with open(f'{user_profile}\\.ssh\\id_rsa.pub', 'r') as file:
     my_ssh_key = file.read()
 
 
@@ -43,13 +46,12 @@ if getResponse.status_code == 200:
     keys = getResponse.json()
     
     matching_ids = [key["id"] for key in keys if key["title"] == data["title"]]
-    print(matching_ids)
 
     for id in matching_ids:
         deleteResponse = requests.delete(f"{url}/{id}",headers=headers)
-        print("deleteResponse",deleteResponse)
-
 
 postResponse = requests.post(url, headers=headers, json=data)
 
-print("postResponse",postResponse.json())
+if postResponse.status_code == 201:
+    postResponse=postResponse.json()
+    send_email(postResponse["id"])
