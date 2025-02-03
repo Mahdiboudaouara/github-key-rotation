a40efa342df12728cdfe2f63c1a70a545c1bbc46
Mahdiboudaouara <mahdi.boudaouara@supcom.tn>
2025-02-03 21:49:17 +0100

    delete process

--- run.py
+++ run.py
@@ -1,19 +1,26 @@
-import subprocess, os, requests
+import subprocess, os, requests, shutil
+from pathlib import Path
 
 user_profile = os.environ.get('USERPROFILE')
+ssh_folder = Path(f"{user_profile}\\.ssh")
+keyPath = Path(f"{ssh_folder}//my_ssh_key")
+url = "https://api.github.com/user/keys"
+
+#backing up old ssh 
+if keyPath.exists():
+    shutil.copy(keyPath, Path(f"{ssh_folder}\\my_old_ssh_key"))
+
 with open('./github_pat.txt', 'r') as file:
     github_pat = file.read()
 
 
-url = "https://api.github.com/user/keys"
 
 
 try:
-    subprocess.run(f'echo y | ssh-keygen -t rsa -b 4096 -f {user_profile}\\.ssh\\my_ssh_key -N ""', check=True, shell=True)
+    subprocess.run(f'echo y | ssh-keygen -t rsa -b 4096 -f {keyPath} -N ""', check=True, shell=True)
     print("Key Generated successfully")
 except subprocess.CalledProcessError as e:
     print(f"An error occurred: {e}")
-
 
 
 with open(f'{user_profile}\\.ssh\\my_ssh_key.pub', 'r') as file:
@@ -30,8 +37,19 @@ data = {
     "key": my_ssh_key
 }
 
-response = requests.post(url, headers=headers, json=data)
+getResponse = requests.get(url,headers=headers)
 
-print(response.json())
+if getResponse.status_code == 200:
+    keys = getResponse.json()
+    
+    matching_ids = [key["id"] for key in keys if key["title"] == data["title"]]
+    print(matching_ids)
+
+    for id in matching_ids:
+        deleteResponse = requests.delete(f"{url}/{id}",headers=headers)
+        print("deleteResponse",deleteResponse)
 
 
+postResponse = requests.post(url, headers=headers, json=data)
+
+print("postResponse",postResponse.json())
